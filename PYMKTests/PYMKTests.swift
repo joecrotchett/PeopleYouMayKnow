2845aedcc94c3d82e810319d90f40f91bbbd08c7
Joe Crotchett <joe.crotchett@gmail.com>
2021-11-20 19:07:07 -0600

    Got tests working

--- PYMKTests/PYMKTests.swift
+++ PYMKTests/PYMKTests.swift
@@ -9,25 +9,35 @@ import XCTest
 @testable import PYMK
 
 class PYMKTests: XCTestCase {
+    
+    private var people = [Person]()
+    
+    // Swift
+    override func setUp() {
+        super.setUp()
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        let exp = expectation(description: "\(#function)\(#line)")
+        
+        let api = JSONAPI(filename: "mock")
+        let dataManager = PeopleDataManager(api: api)
+        
+        dataManager.getPeople { result in
+            switch result {
+            case .success(let people):
+                self.people = people
+                
+            case .failure(let error):
+                XCTFail("Something ain't right: \(error.localizedDescription)")
+            }
+            
+            exp.fulfill()
         }
-    }
 
+        // Wait for the async request to complete
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testJSONParsing() throws {
+        XCTAssertEqual(people.count, 8)
+    }
 }
